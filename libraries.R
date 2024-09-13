dc75537175e98f5f53fdbea44ba72dc99f2fcddd
EduvOliveira <eduvsoliveira@gmail.com>
2024-09-13 16:42:25 -0300

    update

---
+++ libraries.R
@@ -0,0 +1,9 @@
+library(raster)
+library(rgdal)
+library(rgeos)
+library(maps) #mapas simples, eixos, escala, cidades 
+library(mapdata) #base de dados WorldHires e rios
+library(maptools) #Ler ESRI shapefiles 
+library(rworldmap) #outra base de dados de mapas do mundo
+library(mapproj) #Projeções e grids
+library(ggmap) #Gmaps, OSM + mapas baseados em ggplot2
